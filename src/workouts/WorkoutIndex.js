967056d2993aa44facd1ae7ef0396a4e530408d8
Shavonne Holton <shavonne.holton@gmail.com>
2021-07-01 13:27:49 -0700

    added workout log

---
+++ src/workouts/WorkoutIndex.js
@@ -0,0 +1,37 @@
+import React, {useState, useEffect} from 'react';
+import { Container, Row, Col} from 'reactstrap';
+
+const WorkoutIndex = (props) => {
+    const [workouts, setWorkouts] = useState([]);
+    const fetchWorkouts = () => {
+        fetch('http://localhost:3000/log', {
+            method: 'GET',
+            headers: new Headers({
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${props.token}`
+            })
+        }).then((res) => res.json())
+            .then((logData) => {
+                setWorkouts(logData);
+            })
+    };
+
+    useEffect(() => {
+        fetchWorkouts();
+    }, []);
+
+    return(
+        <Container>
+            <Row>
+                <Col md='3'>
+
+                </Col>
+                <Col md='9'>
+                    <h2>Log a workout to see a table. This will be added in later pages.</h2>
+                </Col>
+            </Row>
+        </Container>
+    )
+};
+
+export default WorkoutIndex;
